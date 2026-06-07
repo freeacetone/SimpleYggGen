7bea1b81aae858ecc0e61de65850caebda78ff22
acetone <63557806+freeacetone@users.noreply.github.com>
2026-06-07 15:48:02 +0300

    Bump version to 5.3

--- src/version.h
+++ src/version.h
@@ -6,9 +6,9 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define SYG_VERSION       "5.2"
+#define SYG_VERSION       "5.3"
 #define SYG_VERSION_MAJOR 5
-#define SYG_VERSION_MINOR 2
+#define SYG_VERSION_MINOR 3
 #define SYG_VERSION_PATCH 0
 
 #endif // VERSION_H
